c391a40cfb8ae549d51371104f64c03a77cb3b60
natalie <brocklix@icloud.com>
2022-01-18 22:11:06 -0600

    less sensitive url checker

--- create.js
+++ create.js
@@ -46,12 +46,7 @@ module.exports = async (req, res) => {
 }
 
 function validURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+=]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    var pattern = /^[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&()\[\]//=]*)?/gi
     return !!pattern.test(str);
 }
 
